be612eaebd801d05bae22b05cf3ba2ed150c601f
Антон Шмидт <schmidt.ajax@gmail.com>
2022-01-16 21:42:58 +0700

    Solution (slow)

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <vector>
@@ -7,8 +8,34 @@ using namespace std;
 class Solution {
  public:
   int maxArea(vector<int>& height) {
-    // FIXME
-    return 0;
+    int max_area = 0;
+    if (not height.empty()) {
+      int waterline = *max_element(height.begin(), height.end());
+      while (0 < waterline) {
+        int left = -1;
+        for (int i = 0; i < height.size(); ++i) {
+          if (waterline <= height[i]) {
+            left = i;
+            break;
+          }
+        }
+        if (0 <= left) {
+          int right = -1;
+          for (int k = height.size() - 1; left < k; --k) {
+            if (waterline <= height[k]) {
+              right = k;
+              break;
+            }
+          }
+          if (0 < right) {
+            max_area = max(max_area, (right - left) * waterline);
+          }
+        }
+        --waterline;
+      }
+    }
+
+    return max_area;
   }
 };
 
